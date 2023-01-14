48d59796827a3fc53965a753415300684ee1d166
Arish Kumar <97384467+Jimzical@users.noreply.github.com>
2023-01-14 21:11:59 +0530

    With Background Color Option Now

--- LiveColorASCII.py
+++ LiveColorASCII.py
@@ -10,12 +10,6 @@ import sys
 # allow windows to use color in the terminal
 os.system("color 0a")
 
-
-# setting the ascii characters by surface area
-ascii_characters_by_surface_BW = " `^\",:;Il!i~+_-?tfjrxnuvcz0mwqpdbkhao*#MW&8%B@$"
-ascii_characters_by_surface = [' ', '.', "'", ',', '`', ':', '"', '_', ';', '-', '!', 'I', 'i', 'l', '^', 'r', 'v', '1', 'f', 't', 'j', '~', 'h', 'a', 'A', 'k', 'e', 'C', '4', 'w', 'U', '3', 'X', 'b', 'd', 'p', 'q', 'Z', 'P', '2', 'E', 'H', '0', '5', 'G', 'S', 'O', 'g', 'K', '6', '9', 'D', 'm', 'N', '8', 'R', 'Q', 'B', 'W', 'M', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█', '▉', '▊', '▋', '▌', '▍', '▎', '▏', '▐', '░', '▒', '▓', '&', '%', '#', '@', '$'] 
-
-
 # funct to take a photo using the webcam
 def take_photo(show = False):
     '''
@@ -81,23 +75,31 @@ def convert_to_ascii_art(image,width, height, BW = False):
     return ascii_art
 
 
-def convert_pixel_to_character(pixel,BW = False):
+def convert_pixel_to_character(pixel,BW = False,background = False):
     '''
     ------------------------------------------------
       Convert the pixel to a character            
     ------------------------------------------------
     @param pixel: the pixel as a list of 3 numbers (r,g,b)
     @param BW: if true the photo will be converted to black and white [Default: False]
+    @param background: if true the background will be black [Default: False]
 
     @return: the character
     '''
+
+    # setting the ascii characters by surface area
+    ascii_characters_by_surface_BW = " `^\",:;Il!i~+_-?tfjrxnuvcz0mwqpdbkhao*#MW&8%B@$"
+    ascii_characters_by_surface = [' ', '.', "'", ',', '`', ':', '"', '_', ';', '-', '!', 'I', 'i', 'l', '^', 'r', 'v', '1', 'f', 't', 'j', '~', 'h', 'a', 'A', 'k', 'e', 'C', '4', 'w', 'U', '3', 'X', 'b', 'd', 'p', 'q', 'Z', 'P', '2', 'E', 'H', '0', '5', 'G', 'S', 'O', 'g', 'K', '6', '9', 'D', 'm', 'N', '8', 'R', 'Q', 'B', 'W', 'M', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█', '▉', '▊', '▋', '▌', '▍', '▎', '▏', '▐', '░', '▒', '▓', '&', '%', '#', '@', '$'] 
+
+
     (r, g, b) = pixel
     pixel_brightness = r + g + b
     max_brightness = 255 * 3
     
     if BW:
         ascii_characters_by_surface = ascii_characters_by_surface_BW
-
+    if background:
+        ascii_characters_by_surface = ascii_characters_by_surface[::-1]
     brightness_weight = len(ascii_characters_by_surface) / max_brightness
     index = int(pixel_brightness * brightness_weight) - 1
     return ascii_characters_by_surface[index]
@@ -137,13 +139,14 @@ def fix_rgb(r,g,b,strong = False,Strongthreshold = 150):
                 rgb = 255
     return r,g,b
         
-def rgb_to_escape(colorlist,text):
+def rgb_to_escape(colorlist,text,background = False):
     '''
     ------------------------------------------------
          Convert the rgb values to an escape sequence so add color to the text
     ------------------------------------------------
     @param colorlist: the rgb values as a list
     @param text: the text to add color to
+    @param background: if true the background will be black [Default: False ]
 
     @return: the text with color
     '''
@@ -151,10 +154,17 @@ def rgb_to_escape(colorlist,text):
     r, g, b = colorlist[0],colorlist[1],colorlist[2]
     # fixing the rgb values
     r,g,b = fix_rgb(r,g,b)
-    # printing the text with color
-    print("\033[38;2;{};{};{}m".format(b, g, r) + text + "\033[0m",end='')
-
+    if background:
+        # printing the text with background color
+        print("\033[47;30m" + text + "\033[0m",end='')
+        # q: can i change the text color here to blue?
+        # a: yes you can
+        #
+    else:
+        # printing the text with black color and white background
+        print("\033[38;2;{};{};{}m".format(b,g,r) + text + "\033[0m",end='')
 if __name__ == "__main__":
+    # setting the ascii characters by surface area
 
     # if the user wants Black and White make it BW = True
     BW = False
